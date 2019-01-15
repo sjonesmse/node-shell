ceb5467c1143b6017e043002df9f018aa6232780
Sandy Jones <sjonesmse@gmail.com>
2019-01-15 10:59:21 -0600

    sandy initial

---
+++ bash.js
@@ -0,0 +1,20 @@
+process.stdout.write('prompt > ');
+
+// process.stdin.on('data', data => {
+//   const cmd = data.toString().trim();
+
+//   process.stdout.write('You typed: ' + cmd);
+//   process.stdout.write('\nprompt > ');
+// });
+
+process.stdin.on('data', data => {
+  const cmd = data.toString().trim();
+
+  if (cmd === 'pwd') {
+    process.stdout.write(process.cwd());
+  } else {
+    process.stdout.write('You typed: ' + cmd);
+  }
+
+  process.stdout.write('\nprompt > ');
+});
